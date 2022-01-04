3d3a34725ab42d7479619ddb14cd713632bd25a3
Vahe <info@vahegrickoryhassratian.com>
2022-01-04 18:23:06 +0400

    removed data folder and added new loop

--- filelocker.py
+++ filelocker.py
@@ -1,7 +1,19 @@
 # Modules
-from auth import*
+from auth import *
+import os
+from pathfinder import main_path
+
 
 def main():
+
+    required_directories = ['\\files', '\\data']
+
+    for i in required_directories:
+        try:
+            os.mkdir(main_path + i)
+        except OSError:
+            pass
+
     Login()
 
 
